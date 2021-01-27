7a335287fcae07ebd9b91205d2595c1473ff0a6d
Jeremiah Coholich <jcoholich@gatech.edu>
2021-01-27 16:42:55 -0500

    bug fixes and improvement in AliengoSteps.py

--- gym_aliengo/envs/aliengo_steps.py
+++ gym_aliengo/envs/aliengo_steps.py
@@ -30,12 +30,12 @@ class AliengoSteps(_aliengo_parent.AliengoEnvParent):
         assert row_width > 0.01
         self.row_width = row_width
         self.terrain_height_range = terrain_height_range # +/- half of this value to the height mean 
-        self.terrain_length = 50 #TODO  make this parameter scale with reward because terrain generation takes a lot of time 
+        self.terrain_length = 5 #TODO  make this parameter scale with reward because terrain generation takes a lot of time 
         self.terrain_width = 3 
-        self.terrain_height = terrain_height_range/2. + 0.05 # this is just the mean height of the blocks
+        self.terrain_height = terrain_height_range/2.0 + 0.01 # this is just the mean height of the blocks
 
         self.block_length_range = self.row_width/2. # the mean is set to the same as row_width. 
-        self.ramp_distance = self.terrain_height * 4
+        self.ramp_distance = self.terrain_height * 10
         self.ray_start_height = self.terrain_height + self.terrain_height_range/2. + 1.
 
         self.reset()
@@ -61,28 +61,29 @@ class AliengoSteps(_aliengo_parent.AliengoEnvParent):
         # pick a list of discrete values for heights to only generate a finite number of collision shapes
         n_shapes = 10
         height_values = np.linspace(-self.terrain_height_range/2., self.terrain_height_range/2., n_shapes)
-        length_values = np.linspace(self.row_width - self.block_length_range/2.,
+        length_lb = np.max((self.row_width - self.block_length_range/2., .05))
+        length_values = np.linspace(length_lb,
                                     self.row_width + self.block_length_range/2.,
                                     n_shapes)      
         shapeId = np.zeros(n_shapes, dtype=np.int)
         fake_shapeId = np.zeros(n_shapes, dtype=np.int)
         for i in range(len(length_values)):
-            halfExtents=[length_values[i]/2., self.row_width/2., self.terrain_height/2.] 
+            halfExtents=[length_values[i]/2., self.row_width/2., self.terrain_height_range/2.] 
             shapeId[i] = self.client.createCollisionShape(p.GEOM_BOX, halfExtents=halfExtents)
             fake_shapeId[i] = self.fake_client.createCollisionShape(p.GEOM_BOX, halfExtents=halfExtents)
 
         for row in range(int(ceil(self.terrain_width/self.row_width))):
-            total_len = 0
+            total_len = 0.0
             while total_len < self.terrain_length:
                 i = np.random.randint(0, n_shapes)
                 j = np.random.randint(0, n_shapes)
                 if total_len < self.ramp_distance:
-                    offset = self.terrain_height * (1 - float(total_len)/self.ramp_distance)
+                    offset = self.terrain_height_range * (1 - float(total_len)/self.ramp_distance)
                 else:
                     offset = 0
-                pos = [total_len + length_values[i]/2. + 0.5, # X
+                pos = [total_len + length_values[i]/2.0 + 0.5, # X
                         row * self.row_width - self.terrain_width/2. + self.row_width/2., # Y
-                        height_values[j] - offset + self.terrain_height/2.] # Z
+                        height_values[j] - offset + 0.01] # Z
                 self.client.createMultiBody(baseCollisionShapeIndex=shapeId[i], basePosition=pos)
                 self.fake_client.createMultiBody(baseCollisionShapeIndex=fake_shapeId[i], basePosition=pos)
                 total_len += length_values[i]
@@ -92,7 +93,7 @@ class AliengoSteps(_aliengo_parent.AliengoEnvParent):
         ''' Calculates whether to end current episode due to failure based on current state. '''
 
         quadruped_done, termination_dict = self.quadruped.is_state_terminal(flipping_bounds=[np.pi/2.0]*3, 
-                                height_ub=0.8 + self.terrain_height + self.terrain_height_range/2.) 
+                                height_ub=0.8 + self.terrain_height_range + 0.01) 
         timeout = (self.eps_step_counter >= self.eps_timeout) or \
                     (self.quadruped.base_position[0] >= self.terrain_length - 1.0)
         y_out_of_bounds = not (-self.terrain_width/2. < self.quadruped.base_position[1] < self.terrain_width/2.)
