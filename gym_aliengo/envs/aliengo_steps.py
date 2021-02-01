f93b13cce113e7a3f72c9fe6abf870bc24648b9e
Jeremiah Coholich <jcoholich@gatech.edu>
2021-01-31 23:09:47 -0500

    increased terrain length for steps env

--- gym_aliengo/envs/aliengo_steps.py
+++ gym_aliengo/envs/aliengo_steps.py
@@ -30,7 +30,7 @@ class AliengoSteps(_aliengo_parent.AliengoEnvParent):
         assert row_width > 0.01
         self.row_width = row_width
         self.terrain_height_range = terrain_height_range # +/- half of this value to the height mean 
-        self.terrain_length = 5 #TODO  make this parameter scale with reward because terrain generation takes a lot of time 
+        self.terrain_length = 20  #TODO make this parameter scale with reward because terrain generation takes a lot of time 
         self.terrain_width = 3 
         self.terrain_height = terrain_height_range/2.0 + 0.01 # this is just the mean height of the blocks
 
